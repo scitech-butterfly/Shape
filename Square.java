f752ac85f88342eea3e29da36f6a1b7067602a78
Kashvi Singh <kashvi1401@gmail.com>
2025-03-07 19:10:56 +0530

    Added Area and Perimeter Methods

--- Square.java
+++ Square.java
@@ -2,8 +2,19 @@
 
 class Square extends Shape {
 
+ // Constructor
  public Square(double side) {
   super(side, 0, 0, 4); // for super class Shape's parameterized constructor
+ }
+
+ // Method to calculate area
+ public double calculateArea() {
+  return dim_one * dim_one; // Area = (side * side)
+ }
+
+ // Method to calculate perimeter
+ public double calculatePerimeter() {
+  return 4 * dim_one; // Perimeter = (4 * side)
  }
 
 }
