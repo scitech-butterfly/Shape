eb78cd99561ef6675fc0745802b92fd79fabbeb6
Kashvi Singh <kashvi1401@gmail.com>
2025-03-07 21:58:17 +0530

    Created EquilateralPyramid.java

---
+++ EquilateralPyramid.java
@@ -0,0 +1,9 @@
+// EquilateralPyramid.java
+
+class EquilateralPyramid extends Shape {
+
+ // EquilateralPyramid Constructor
+ public EquilateralPyramid(double base, double height) {
+  super(base, height, 0, 4);
+ }
+}
