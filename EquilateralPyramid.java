d9e7492db5d0b98b629c3bd7c9ee9a4372410e1d
Kashvi Singh <kashvi1401@gmail.com>
2025-03-07 22:05:53 +0530

    Added method to calculate area

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -6,4 +6,10 @@ class EquilateralPyramid extends Shape {
  public EquilateralPyramid(double base, double height) {
   super(base, height, 0, 4);
  }
+
+ // Method to calculate area
+ public double calculateArea() {
+  return (dim_one * dim_one) + (2 * dim_one * Math.sqrt((dim_one * dim_one / 4) + (dim_two * dim_two)));
+ }
+
 }
