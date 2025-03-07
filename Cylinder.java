c04e479edd053db47c60d4a0a87ca865eeb75a6b
Kashvi Singh <kashvi1401@gmail.com>
2025-03-07 19:21:13 +0530

    Implemented Cylinder Constructor

--- Cylinder.java
+++ Cylinder.java
@@ -1,5 +1,10 @@
 // Cylinder.java
 
-class Cylinder {
+class Cylinder extends Shape{
+
+ // Cylinder constructor
+ public Cylinder(double radius, double height) {
+  super(radius, height, 0, 0);
+ }
 
 }
