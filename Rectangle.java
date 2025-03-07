802f64c693f6011f69859a7c24cbdf6a62824ba7
Kashvi Singh <kashvi1401@gmail.com>
2025-03-07 19:09:30 +0530

    Implemented Square Method for Shape

--- Rectangle.java
+++ Rectangle.java
@@ -2,11 +2,11 @@
 
 class Rectangle extends Shape {
  public Rectangle(double length, double width) {
-  super(length, width, 0, 4); // for super class Shape
+  super(length, width, 0, 4); // for super class Shape's parameterized constructor
  }
 
  // Method to calculate area
- public double calculateArea() { 
+ public double calculateArea() {
   return dim_one * dim_two; // Area = (length * breadth)
  }
 
--- Square.java
+++ Square.java
@@ -2,4 +2,8 @@
 
 class Square extends Shape {
 
+ public Square(double side) {
+  super(side, 0, 0, 4); // for super class Shape's parameterized constructor
+ }
+
 }
