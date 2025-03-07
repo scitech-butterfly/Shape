f8474616396ac8dffcbd814c04d01eb2f87b8408
Kashvi Singh <kashvi1401@gmail.com>
2025-03-07 12:07:21 +0530

    Created Shape Constructors

--- Shape.java
+++ Shape.java
@@ -1,5 +1,21 @@
 // Shape.java
 
-class Shape{
- 
+abstract class Shape {
+ protected double dim_one, dim_two, dim_three;
+ int num_sides;
+
+ // Zero parameter constructor when there is no input
+ public Shape() {
+  dim_one = dim_two = dim_three = 0;
+  num_sides = 0;
+ }
+
+ // Parameterized constructor when there is input for dimensions
+ public Shape(double dim_one, double dim_two, double dim_three, int num_sides) {
+  this.dim_one = dim_one;
+  this.dim_two = dim_two;
+  this.dim_three = dim_three;
+  this.num_sides = num_sides;
+ }
+
 }
