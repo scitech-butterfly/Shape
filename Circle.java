8060fb3a375f835f20b425f1d77622304b3d5954
Kashvi Singh <kashvi1401@gmail.com>
2025-03-07 19:15:13 +0530

    Added Area and Perimeter Methods

--- Circle.java
+++ Circle.java
@@ -1,9 +1,21 @@
 // Circle.java
 
+import java.lang.Math;
+
 class Circle extends Shape {
 
  // Circle Constructor
  public Circle(double radius) {
   super(radius, 0, 0, 0);
+ }
+
+ // Method to calculate area
+ public double calculateArea() {
+  return Math.PI * Math.pow(dim_one, 2);
+ }
+
+ // Method to calculate perimeter (circumference)
+ public double calculatePerimeter() {
+  return 2 * Math.PI * dim_one;
  }
 }
